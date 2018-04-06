10e44d56576176b7d8fbdb8294cb1f1da3209755
fherbine <felix.herbinet@yahoo.com>
2018-04-06 16:38:26 +0200

    Adding SQL - Ex3

---
+++ sql/ex3.php
@@ -0,0 +1,23 @@
+<?php
+	include_once("../db_auth.php");
+	$bdd = db_auth('localhost', 'test');
+	if ($_POST && isset($_POST['title']) && isset($_POST['content']) && $_POST['submit'] === "OK")
+	{
+		$req0 = $bdd->prepare('INSERT INTO news(titre, contenu) VALUES (:titre, :contenu)');
+		$req0->execute(array('titre' => htmlspecialchars($_POST['title']), 'contenu' => htmlspecialchars($_POST['content'])));
+		header('Location: ex1.php');
+	}
+?>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Add an article</title>
+	</head>
+	<body>
+		<form method="post" action="ex3.php">
+			<input type="text" name="title" placeholder="Title" required/>
+			<textarea name="content" placeholder="Content of the article ..." required></textarea>
+			<input type="submit" name="submit" value="OK"/>
+		</form>
+	</body>
+</html>
